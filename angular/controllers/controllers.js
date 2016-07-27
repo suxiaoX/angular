e765b6e0659f7b52cb47aa0c34b2ba25cbf5deee
suxiaoX <zhaozichao@ucfgroup.com>
2016-07-27 10:46:28 +0800

    动画

--- angular/controllers/controllers.js
+++ angular/controllers/controllers.js
@@ -5,6 +5,7 @@ bookStoreCtrls.controller('HelloCtrl', ['$scope',
         $scope.greeting = {
             text: 'Hello'
         };
+        $scope.pageClass = "hello"
     }
 ]);
 
@@ -14,7 +15,9 @@ bookStoreCtrls.controller('BookListCtrl', ['$scope',
         	{title:"《Ext江湖》",author:"大漠穷秋"},
         	{title:"《ActionScript游戏设计基础（第二版）》",author:"大漠穷秋"},
         	{title:"《用AngularJS开发下一代WEB应用》",author:"大漠穷秋"}
-        ]
+        ];
+        
+        $scope.pageClass = "list";
     }
 ]);
 
